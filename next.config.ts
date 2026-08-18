9c370bd792dc50782a43dfa4b14d45a1d1c9bf32
innovaservicesgroup <admin@innovaservicesgroup.com.au>
2026-08-18 10:14:10 +0800

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["puppeteer-core", "@sparticuz/chromium", "puppeteer"],
+  outputFileTracingIncludes: {
+    "/api/generate-pdf": ["./node_modules/@sparticuz/chromium/**"],
+  },
 };
 
 export default nextConfig;
